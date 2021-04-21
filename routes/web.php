cb9b40f8b3f6a0122202d611de26f4f052fd8f76
shota.tabata <yotubato4289@gmail.com>
2021-04-21 13:51:05 +0000

    routes-controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//トップページをタスク一覧のページにする
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
